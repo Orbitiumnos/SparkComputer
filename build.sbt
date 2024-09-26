2a823e16524d35d792702bf374d9a54e882c26f9
Nikolay Fedorov <fedonick1@gmail.com>
2024-09-26 23:26:13 +0300

    first commit

---
+++ build.sbt
@@ -0,0 +1,16 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.15"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "sparktutorial",
+    idePackagePrefix := Some("com.example.sparktutorial")
+  )
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.5.0",
+  "org.apache.spark" %% "spark-sql" % "3.5.0"
+)
+
+fork := true
---
+++ src/main/scala/SparkExampleMain.scala
@@ -0,0 +1,22 @@
+package com.example.sparktutorial
+
+
+import com.example.sparktutorial.Analysis.calculateAverageTipByPickupLocation
+
+
+object SparkExampleMain extends App {
+  val spark = createSparkSession("Spark test", isLocal = true)
+
+
+  val (inputPath, outputPath) = parseArgs(args = args)
+  val data = spark.read.parquet(s"${inputPath}/*.parquet")
+
+
+  val analysisResult = calculateAverageTipByPickupLocation(data = data)
+
+  println("Start")
+
+  analysisResult.write.option("header", "true").csv(outputPath)
+
+  println("Finish")
+}
