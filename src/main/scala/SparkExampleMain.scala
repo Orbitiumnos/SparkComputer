00ad77b50893bdbce90d711e7fd3136d8ec2a90b
Nikolay Fedorov <fedonick1@gmail.com>
2024-09-27 00:07:30 +0300

    fix

--- src/main/scala/SparkExampleMain.scala
+++ src/main/scala/SparkExampleMain.scala
@@ -5,16 +5,16 @@ import com.example.sparktutorial.Analysis.calculateAverageTipByPickupLocation
 
 
 object SparkExampleMain extends App {
-  val spark = createSparkSession("Spark test", isLocal = true)
-
-
-  val (inputPath, outputPath) = parseArgs(args = args)
-  val data = spark.read.parquet(s"${inputPath}/*.parquet")
-
-
-  val analysisResult = calculateAverageTipByPickupLocation(data = data)
 
   println("START")
+
+  val spark = createSparkSession("Spark test", isLocal = true)
+
+  val (inputPath, outputPath) = parseArgs(args = args)
+
+  val data = spark.read.parquet(s"${inputPath}/*.parquet")
+
+  val analysisResult = calculateAverageTipByPickupLocation(data = data)
 
   analysisResult.write.option("header", "true").csv(outputPath)
 
